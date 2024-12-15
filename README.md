a01f96882b3677abe4a47fb4999d530a212ac196
StephanPraetsch <stephan.praetsch@googlemail.com>
2024-12-15 01:55:22 +0100

    print some events

--- README.md
+++ README.md
@@ -7,5 +7,5 @@ only manual installation, nowhere published yet
 copy this folder `wow-wishlist` into your local wow folder `World of Warcraft\_retail_\Interface\AddOns\wow-wishlist`, e.g. `D:\Blizzard\World of Warcraft\_retail_\Interface\AddOns/wow-wishlist`
 
 ```shell
-xcopy . 'D:\Blizzard\World of Warcraft\_retail_\Interface\AddOns/wow-wishlist'
+xcopy /Y . 'D:\Blizzard\World of Warcraft\_retail_\Interface\AddOns/wow-wishlist'
 ```
--- main.lua
+++ main.lua
@@ -1,1 +1,24 @@
 print("hello wishlist")
+
+local frame = CreateFrame("Frame")
+
+function frame:OnEvent(event, ...)
+    self[event](self, event, ...)
+end
+
+function frame:ADDON_LOADED(event, addOnName)
+    print("new event", event, addOnName)
+end
+
+function frame:PLAYER_ENTERING_WORLD(event, isLogin, isReload)
+    print("new event", event, isLogin, isReload)
+end
+
+function frame:CHAT_MSG_CHANNEL(event, text, playerName, _, channelName)
+    print("new event", event, text, playerName, channelName)
+end
+
+frame:RegisterEvent("ADDON_LOADED")
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+frame:RegisterEvent("CHAT_MSG_CHANNEL")
+frame:SetScript("OnEvent", frame.OnEvent)
