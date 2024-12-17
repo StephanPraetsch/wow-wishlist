a8cb5ca50328cf5347d28c31c9f13b94a1389bc1
StephanPraetsch <stephan.praetsch@googlemail.com>
2024-12-17 23:17:53 +0100

    move code to OptionsPanel.lua

--- OptionsPanel.lua
+++ OptionsPanel.lua
@@ -1,14 +1,45 @@
 local addonName, addonData = ...
 
-addonData.OptionsPanel = {}
+local options = {
+    reportJump = false,
+    categoryID = nil,
+    panel = nil,
+}
+addonData.OptionsPanel = options
+
 
 local function init()
-    print("OptionsPanel init")
+    print("OptionsPanel initializing")
+    panel = CreateFrame("Frame")
+    options.panel = panel
+    panel.name = "HelloWorld"
+
+    local cb = CreateFrame("CheckButton", nil, panel, "InterfaceOptionsCheckButtonTemplate")
+    cb:SetPoint("TOPLEFT", 20, -20)
+    cb.Text:SetText("Print when you jump")
+    cb:HookScript("OnClick", function(_, btn, down)
+        options.reportJump = cb:GetChecked()
+    end)
+    cb:SetChecked(options.reportJump)
+
+    local btn = CreateFrame("Button", nil, panel, "UIPanelButtonTemplate")
+    btn:SetPoint("TOPLEFT", cb, 0, -40)
+    btn:SetText("Click me")
+    btn:SetWidth(100)
+    btn:SetScript("OnClick", function()
+        print("You clicked me!")
+    end)
+
+    local category = Settings.RegisterCanvasLayoutCategory(panel, ADDON_NAME)
+    Settings.RegisterAddOnCategory(category)
+    options.categoryID = category:GetID()
+    print("OptionsPanel initialized")
 end
 addonData.OptionsPanel.init = init
 
 local function show()
-    print("OptionsPanel show")
+    print("OptionsPanel show", options.categoryID)
+    Settings.OpenToCategory(options.categoryID)
 end
 addonData.OptionsPanel.show = show
 
--- main.lua
+++ main.lua
@@ -41,31 +41,6 @@ frame:SetScript("OnEvent", frame.OnEvent)
 function frame:InitializeOptions()
     print("frame:InitializeOptions")
     OptionsPanel.init()
-
-    self.panel = CreateFrame("Frame")
-    self.panel.name = "HelloWorld"
-
-    local cb = CreateFrame("CheckButton", nil, self.panel, "InterfaceOptionsCheckButtonTemplate")
-    cb:SetPoint("TOPLEFT", 20, -20)
-    cb.Text:SetText("Print when you jump")
-    cb:HookScript("OnClick", function(_, btn, down)
-        self.db.reportJump = cb:GetChecked()
-    end)
-    cb:SetChecked(self.db.reportJump)
-
-    local btn = CreateFrame("Button", nil, self.panel, "UIPanelButtonTemplate")
-    btn:SetPoint("TOPLEFT", cb, 0, -40)
-    btn:SetText("Click me")
-    btn:SetWidth(100)
-    btn:SetScript("OnClick", function()
-        print("You clicked me!")
-    end)
-
-    print("add category")
-    local category = Settings.RegisterCanvasLayoutCategory(self.panel, ADDON_NAME)
-    Settings.RegisterAddOnCategory(category)
-    print("added category", category:GetID())
-    frame.panel.categoryID = category:GetID()
 end
 
 SLASH_HELLOW1 = "/wl"
@@ -73,8 +48,5 @@ SLASH_HELLOW2 = "/wishlist"
 
 SlashCmdList.HELLOW = function(msg, editBox)
     print("slash command was called", msg)
-    local settingsCategoryID = frame.panel.categoryID
-    print("open category", settingsCategoryID)
-    Settings.OpenToCategory(settingsCategoryID)
     OptionsPanel.show()
 end
