98662bdf4211aa96bfd9afc09da24f67daea0d53
StephanPraetsch <stephan.praetsch@googlemail.com>
2024-12-18 01:35:43 +0100

    save reportJump in Database in OptionsPanel

--- OptionsPanel.lua
+++ OptionsPanel.lua
@@ -1,26 +1,21 @@
 local addonName, addonData = ...
 
-local options = {
-    reportJump = false,
-    categoryID = nil,
-    panel = nil,
-}
-addonData.OptionsPanel = options
+addonData.OptionsPanel = {}
 
+local state = {}
 
 local function init()
     print("OptionsPanel initializing")
     panel = CreateFrame("Frame")
-    options.panel = panel
     panel.name = "HelloWorld"
 
     local cb = CreateFrame("CheckButton", nil, panel, "InterfaceOptionsCheckButtonTemplate")
     cb:SetPoint("TOPLEFT", 20, -20)
     cb.Text:SetText("Print when you jump")
     cb:HookScript("OnClick", function(_, btn, down)
-        options.reportJump = cb:GetChecked()
+        Database.reportJump = cb:GetChecked()
     end)
-    cb:SetChecked(options.reportJump)
+    cb:SetChecked(Database.reportJump)
 
     local btn = CreateFrame("Button", nil, panel, "UIPanelButtonTemplate")
     btn:SetPoint("TOPLEFT", cb, 0, -40)
@@ -32,14 +27,14 @@ local function init()
 
     local category = Settings.RegisterCanvasLayoutCategory(panel, ADDON_NAME)
     Settings.RegisterAddOnCategory(category)
-    options.categoryID = category:GetID()
-    print("OptionsPanel initialized")
+    state.categoryID = category:GetID()
+    print("OptionsPanel initialized", state.categoryID)
 end
 addonData.OptionsPanel.init = init
 
 local function show()
-    print("OptionsPanel show", options.categoryID)
-    Settings.OpenToCategory(options.categoryID)
+    print("OptionsPanel show", state.categoryID)
+    Settings.OpenToCategory(state.categoryID)
 end
 addonData.OptionsPanel.show = show
 
