9ce38fcf231aa7a3149513d68e4aa735033486d0
StephanPraetsch <stephan.praetsch@googlemail.com>
2024-12-25 17:20:28 +0100

    mv functions into OptionsPanel object

--- OptionsPanel.lua
+++ OptionsPanel.lua
@@ -1,36 +1,36 @@
 local addonName, addonData = ...
 
-addonData.OptionsPanel = {}
-
 local state = {}
 
-local function init()
-    panel = CreateFrame("Frame")
-    panel.name = "HelloWorld"
+addonData.OptionsPanel = {
 
-    local cb = CreateFrame("CheckButton", nil, panel, "InterfaceOptionsCheckButtonTemplate")
-    cb:SetPoint("TOPLEFT", 20, -20)
-    cb.Text:SetText("Print when you jump")
-    cb:HookScript("OnClick", function(_, btn, down)
-        Database.reportJump = cb:GetChecked()
-    end)
-    cb:SetChecked(Database.reportJump)
+    init = function()
+        panel = CreateFrame("Frame")
+        panel.name = "Wishlist Options"
 
-    local btn = CreateFrame("Button", nil, panel, "UIPanelButtonTemplate")
-    btn:SetPoint("TOPLEFT", cb, 0, -40)
-    btn:SetText("Click me")
-    btn:SetWidth(100)
-    btn:SetScript("OnClick", function()
-        print("You clicked me!")
-    end)
+        local cb = CreateFrame("CheckButton", nil, panel, "InterfaceOptionsCheckButtonTemplate")
+        cb:SetPoint("TOPLEFT", 20, -20)
+        cb.Text:SetText("Print when you jump")
+        cb:HookScript("OnClick", function(_, btn, down)
+            Database.reportJump = cb:GetChecked()
+        end)
+        cb:SetChecked(Database.reportJump)
 
-    local category = Settings.RegisterCanvasLayoutCategory(panel, ADDON_NAME)
-    Settings.RegisterAddOnCategory(category)
-    state.categoryID = category:GetID()
-end
-addonData.OptionsPanel.init = init
+        local btn = CreateFrame("Button", nil, panel, "UIPanelButtonTemplate")
+        btn:SetPoint("TOPLEFT", cb, 0, -40)
+        btn:SetText("Click me")
+        btn:SetWidth(100)
+        btn:SetScript("OnClick", function()
+            print("You clicked me!")
+        end)
 
-local function show()
-    Settings.OpenToCategory(state.categoryID)
-end
-addonData.OptionsPanel.show = show
+        local category = Settings.RegisterCanvasLayoutCategory(panel, ADDON_NAME)
+        Settings.RegisterAddOnCategory(category)
+        state.categoryID = category:GetID()
+    end,
+
+    show = function()
+        Settings.OpenToCategory(state.categoryID)
+    end,
+
+}
