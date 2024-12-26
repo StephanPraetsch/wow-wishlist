81c2c724f086f803c41b86a9c702777bf12ccca5
StephanPraetsch <stephan.praetsch@googlemail.com>
2024-12-26 19:22:10 +0100

    clean up event handling and debug prints

--- main.lua
+++ main.lua
@@ -6,43 +6,37 @@ ADDON_NAME = "wow-wishlist"
 
 local frame = CreateFrame("Frame")
 
-function frame:OnEvent(event, addOnName)
+function frame:OnEvent(event, ...)
+    if DEBUG then
+        print("onEvent", event, ...)
+    end
+    self[event](self, event, ...)
+end
+
+function frame:ADDON_LOADED(event, addOnName)
     if addOnName == ADDON_NAME then
-        self:InitializeOptions()
+        OptionsPanel.init()
         hooksecurefunc("JumpOrAscendStart", function()
             if Database.reportJump then
-                print("Your character jumped.")
+                Database.jumps = (Database.jumps or 0) + 1
+                print("Your character jumped " .. Database.jumps .. " times")
             end
         end)
     end
 end
 
-function frame:ADDON_LOADED(event, addOnName)
-    print("new ADDON_LOADED event", event, addOnName)
-    if addOnName == ADDON_NAME then
-        Database.sessions = (Database.sessions or 0) + 1
-        print("You loaded this addon " .. Database.sessions .. " times")
-    end
-end
-
 function frame:PLAYER_ENTERING_WORLD(event, isLogin, isReload)
-    print("new PLAYER_ENTERING_WORLD event", event, isLogin, isReload)
+    --print("new PLAYER_ENTERING_WORLD event", event, isLogin, isReload)
 end
 
 function frame:CHAT_MSG_CHANNEL(event, text, playerName, _, channelName)
-    print("new CHAT_MSG_CHANNEL event", event, text, playerName, channelName)
-    Database.numberOfChatMessages = (Database.numberOfChatMessages or 0) + 1
-    print("that was the", Database.numberOfChatMessages, "chat message")
+    --print("new CHAT_MSG_CHANNEL event", event, text, playerName, channelName)
 end
 
 frame:RegisterEvent("ADDON_LOADED")
 frame:RegisterEvent("PLAYER_ENTERING_WORLD")
-frame:RegisterEvent("CHAT_MSG_CHANNEL")
+--frame:RegisterEvent("CHAT_MSG_CHANNEL")
 frame:SetScript("OnEvent", frame.OnEvent)
-
-function frame:InitializeOptions()
-    OptionsPanel.init()
-end
 
 SLASH_HELLOW1 = "/wl"
 SLASH_HELLOW2 = "/wishlist"
